a842c7738f69bf2d38eb7b99abc1464de61ba451
Christos Karagiorgos <k.xrhstos095@gmail.com>
2025-12-12 04:15:30 +0200

    update deploy script

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+
+set -e # exit on error
+set -f # disable globbing
 
 ################################################################################
 #                                                                              #
@@ -8,9 +10,9 @@ set -e
 ################################################################################
 
 # get variables from codebuild
-eval "$(aws codebuild batch-get-projects --names "blog-codebuild" \
-  --query "projects[0].environment.environmentVariables" \
-  --output json | jq -r '.[] | "export \(.name)=\"\(.value)\""')"
+CODEBUILD_ENV=$(aws codebuild batch-get-projects --names "blog-codebuild" --query "projects[0].environment.environmentVariables")
+echo "$CODEBUILD_ENV" | jq -r '.[] | "\(.name)=\(.value)"' > .env
+eval "$(awk -F= '{print "export "$1"=\""$2"\""}' .env)"
 
 export IMAGE_TAG="latest"
 export FRONTEND="$ECR_URI/frontend"
@@ -25,10 +27,16 @@ export BACKEND="$ECR_URI/backend"
 # login
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
 
+# remove old images
+docker rm -f backend frontend 2>/dev/null || true
+
 # get last images
 docker pull $BACKEND
 docker pull $FRONTEND
 
-# start docker images
-docker compose -f ../docker-compose.yml down
-docker compose -f ../docker-compose.yml up -d
+# run images
+docker run -d --name backend  --env-file .env -e PORT=$BACKEND_PORT  -e HOST=$BACKEND_HOST  -p $BACKEND_PORT:$BACKEND_PORT   $FRONTEND
+docker run -d --name frontend --env-file .env -e PORT=$FRONTEND_PORT -e HOST=$FRONTEND_HOST -p $FRONTEND_PORT:$FRONTEND_PORT $FRONTEND
+
+# remove env variables
+rm .env
